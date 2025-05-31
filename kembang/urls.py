befc761d0658a3d1478c6bacdea6289859c2c47e
farhan <farhanhafidzan83@gmail.com>
2025-06-01 00:48:42 +0700

    CYBER PUNK

--- kembang/urls.py
+++ kembang/urls.py
@@ -16,27 +16,27 @@ urlpatterns = [
     path('semua/', views.semua_pengajuan, name='semua_pengajuan'),
     
     path('pengajuan-akta-kematian/', views.pengajuan_akta_kematian, name='pengajuan_akta_kematian'),
-    path('admin/akta-kematian/', views.daftar_pengajuan_akta_kematian, name='daftar_pengajuan_akta_kematian'),
-    path('admin/akta-kematian/<int:pk>/', views.detail_pengajuan_akta_kematian, name='detail_pengajuan_akta_kematian'),
+    path('kematian/akta-kematian/', views.daftar_pengajuan_akta_kematian, name='daftar_pengajuan_akta_kematian'),
+    path('kematian/akta-kematian/<int:pk>/', views.detail_pengajuan_akta_kematian, name='detail_pengajuan_akta_kematian'),
     # Akta Kelahiran
     path('pengajuan/akta-kelahiran/', views.pengajuan_akta_kelahiran, name='pengajuan_akta_kelahiran'),
-    path('admin/akta-kelahiran/', views.daftar_pengajuan_kelahiran, name='daftar_pengajuan_kelahiran'),
-    path('admin/akta-kelahiran/<int:pk>/', views.detail_pengajuan_kelahiran, name='detail_pengajuan_kelahiran'),
+    path('kelahiran/akta-kelahiran/', views.daftar_pengajuan_kelahiran, name='daftar_pengajuan_kelahiran'),
+    path('kelahiran/akta-kelahiran/<int:pk>/', views.detail_pengajuan_kelahiran, name='detail_pengajuan_kelahiran'),
 
     # Pindah Datang
     path('pengajuan/pindah-datang/', views.pengajuan_pindah_datang, name='pengajuan_pindah_datang'),
-    path('admin/pindah-datang/', views.daftar_pindah_datang, name='daftar_pengajuan_pindah_datang'),
-    path('admin/pindah-datang/<int:pk>/', views.detail_pindah_datang, name='detail_pengajuan_pindah_datang'),
+    path('datang/pindah-datang/', views.daftar_pindah_datang, name='daftar_pengajuan_pindah_datang'),
+    path('datang/pindah-datang/<int:pk>/', views.detail_pindah_datang, name='detail_pengajuan_pindah_datang'),
 
     # Pindah Keluar
     path('pengajuan/pindah-keluar/', views.pengajuan_pindah_keluar, name='pengajuan_pindah_keluar'),
-    path('admin/pindah-keluar/', views.daftar_pindah_keluar, name='daftar_pengajuan_pindah_keluar'),
-    path('admin/pindah-keluar/<int:pk>/', views.detail_pindah_keluar, name='detail_pengajuan_pindah_keluar'),
+    path('keluar/pindah-keluar/', views.daftar_pindah_keluar, name='daftar_pengajuan_pindah_keluar'),
+    path('keluar/pindah-keluar/<int:pk>/', views.detail_pindah_keluar, name='detail_pengajuan_pindah_keluar'),
     
     # sktm
     path('pengajuan/sktm/', views.pengajuan_sktm, name='pengajuan_sktm'),
-    path('admin/sktm/', views.daftar_sktm, name='daftar_pengajuan_sktm'),
-    path('admin/sktm/<int:pk>/', views.detail_sktm, name='detail_pengajuan_sktm'),
+    path('sktm/sktm/', views.daftar_sktm, name='daftar_pengajuan_sktm'),
+    path('sktm/sktm/<int:pk>/', views.detail_sktm, name='detail_pengajuan_sktm'),
     
     # domisili
     path('domisili/pengajuan/', views.pengajuan_domisili, name='pengajuan_domisili'),
@@ -50,7 +50,7 @@ urlpatterns = [
 
     # urls.py
     path('cek-status/', views.cek_status_surat, name='cek_status'),
-    path('admin/dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.register, name='register'),
